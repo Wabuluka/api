64b2d2a4cecb991b166d16410e3bd7a3f060b80a
Wabuluka <dwabuluka@gmail.com>
2018-09-16 10:05:15 +0300

    [(feature) #160536298] the user can now view the order buy orderId

--- run.py
+++ run.py
@@ -1,27 +1,44 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
+from flask import Flask, jsonify, abort
+
+app = Flask(__name__)
+orders = [
+    {
+        'orderId': 1,
+        'orderTitle': u'Ghee',
+        'orderDescription': u'Just what i want'
+    },
+    {
+        'orderId': 2,
+        'orderTitle': u'Corn',
+        'orderDescription': u'I love corn too'
+    },
+    {
+        'orderId': 3,
+        'orderTitle': u'Milk',
+        'orderDescription': u'I took lots of milk'
+    }
+]
+#home page of the api
 
 
-class Config(object):
-    DEBUG = False
-    TESTING = False
-    CSRF_ENABLED = True
-    SECRET_KEY = 'this-really-needs-to-be-changed'
+@app.route('/')
+def index():
+    return "Hello"
+
+#displaying all the orders in the list
 
 
-class ProductionConfig(Config):
-    DEBUG = False
+@app.route('/api/v1/orders', methods=['GET'])
+def getOrders():
+    return jsonify({'orders': orders})
 
+#returning the data of a single order
+@app.route('/api/v1/orders/<int:id>', methods=['GET'])
+def getOrder(id):
+    order = [order for order in orders if order['orderId'] == id]
+    if len(order) == 0:
+        abort(404)
+    return jsonify({'order': order[0]})
 
-class StagingConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
-
-class DevelopmentConfig(Config):
-    DEVELOPMENT = True
-    DEBUG = True
-
-
-class TestingConfig(Config):
-    TESTING = True
+if __name__ == '__main__':
+    app.run(debug=True)

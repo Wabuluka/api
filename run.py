430cb5b082a4c6ebd71e69e6d7a3679849dfd7bc
Wabuluka <dwabuluka@gmail.com>
2018-09-16 10:27:44 +0300

    [ feature #160502843] user is able to update a record using put

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify, abort, request
 
 app =  Flask(__name__)
 
@@ -40,7 +40,29 @@ def getOrder(id):
     if len(order) == 0:
         abort(404)
     return jsonify({'order': order[0]})
+#put
 
+
+@app.route('/api/v1/orders/<int:id>', methods=['PUT'])
+def updateOrder(id):
+    order = [order for order in orders if order['orderId'] == id]
+    if len(order) == 0:
+        abort(404)
+
+    if not request.json:
+        abort(400)
+
+    if 'orderTitle' in request.json and type(request.json['orderTitle']) != unicode:
+        abort(400)
+
+    if 'orderDescription' in request.json and type(request.json['orderDescription']) is not unicode:
+        abort(400)
+
+    order[0]['orderTitle'] = request.json.get(
+        'orderTitle', order[0]['orderTitle'])
+    order[0]['orderDescription'] = request.json.get(
+        'orderDescription', order[0]['orderDescription'])
+    return jsonify({'order': order[0]})
 
 if __name__ == '__main__':
     app.run(debug=True)

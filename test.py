757c37d60c8b17c4b7bc38dbc4895d8af4902b99
Wabuluka <dwabuluka@gmail.com>
2018-09-22 20:09:21 +0300

    test update

--- test.py
+++ test.py
@@ -5,31 +5,35 @@ import json
 
 class TestRun(unittest.TestCase):
     """Class to test the methods in the run module"""
-    client = run.app.test_client
+    #client = run.app.test_client
+
+    def setUp(self):
+       self.app = run.app.test_client()
 
     def test_hello(self):
         #testing if the homepage can be reached
-        self.client = run.app.test_client
+        #self.client = run.app.test_client
         #self.app = run.app.test_client()
+        pass
 
     def test_get_orders(self):
         """testing for all orders"""
-        all_orders = self.client().get('api/v1/orders')
+        all_orders = self.app.get('api/v1/orders')
         self.assertEqual(all_orders.status_code, 200)
 
     def test_empty_order(self):
         """testing for when no order is placed"""
-        empty_order = self.client().get('')
+        empty_order = self.app.get('')
         self.assertEqual(empty_order.status_code, 404)
 
     def test_get_order(self):
         """fetching only one order test"""
-        one_order = self.client().get('api/v1/orders/1')
+        one_order = self.app.get('api/v1/orders/1')
         self.assertEqual(one_order.status_code, 200)
 
     def test_adding_order(self):
         """adding an order test"""
-        added_order = self.client().post('api/v1/orders', content_type="application/json", data=json.loads(dict(orderId=1, orderTitle="Ghee" ,orderDescription="Just what i want")))
+        added_order = self.app.post('api/v1/orders', content_type="application/json", data=json.loads(dict(orderId=1, orderTitle="Ghee" ,orderDescription="Just what i want")))
         call = json.dumps(added_order.data.decode("utf8"))
         self.assertIn('Ordered', call)
         self.assertIsInstance(call, dict)

eb35eef3b3adf1b66a8a5029796f46de9f590521
Wabuluka <dwabuluka@gmail.com>
2018-09-22 13:48:20 +0300

    test update

--- test.py
+++ test.py
@@ -8,13 +8,33 @@ class TestRun(unittest.TestCase):
     
     def test_hello(self):
         #testing if the homepage can be reached
-        pass
+        self.client = run.app.test_client
 
-    def test_getOrders(self):
-        #testing if the orders exist
-        self.assertFalse(run.getOrders())
-        self.assertEqual(len(run.getOrders()), 0)
-        #rep = client.post('/api/v1/orders', data=json.dumps('orders'))
+    def test_get_orders(self):
+        """testing for all orders"""
+        all_orders = self.client().get('api/v1/orders')
+        self.assertEqual(all_orders.status_code, 200)
+
+    def test_empty_order(self):
+        """testing for when no order is placed"""
+        empty_order = self.client().get('')
+        self.assertEqual(empty_order.status_code, 404)
+
+    def test_get_order(self):
+        """fetching only one order test"""
+        one_order = self.client().get('api/v1/orders/1')
+        self.assertEqual(one_order.status_code, 200)
+
+    def test_adding_order(self):
+        """adding an order test"""
+        added_order = self.client().post('api/v1/orders', content_type="application/json", data=json.loads(dict(orderId=1, orderTitle="Ghee" ,orderDescription="Just what i want")))
+        call = json.dumps(added_order.data.decode("utf8"))
+        self.assertIn('Ordered', call)
+        self.assertIsInstance(call, dict)
+        self.assertEqual(added_order.status_code, 200)
+        self.assertTrue(added_order.json["Ordered"])
+    
+
 
 
 if __name__ == '__main__':

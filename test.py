99faa3d7274833699e645ecaf97811d10e171e2e
Wabuluka <dwabuluka@gmail.com>
2018-09-24 17:49:12 +0300

    testing methods revisited

--- test.py
+++ test.py
@@ -10,26 +10,26 @@ class TestRun(unittest.TestCase):
     def setUp(self):
        self.app = run.app.test_client()
 
-    # def test_hello(self):
-    #     #testing if the homepage can be reached
-    #     #self.client = run.app.test_client
-    #     #self.app = run.app.test_client()
-    #     pass
+    def test_hello(self):
+        #testing if the homepage can be reached
+        #self.client = run.app.test_client
+        #self.app = run.app.test_client()
+        pass
 
-    # def test_get_orders(self):
-    #     """testing for all orders"""
-    #     all_orders = self.app.get('api/v1/orders')
-    #     self.assertEqual(all_orders.status_code, 200)
+    def test_get_orders(self):
+        """testing for all orders"""
+        all_orders = self.app.get('api/v1/orders')
+        self.assertEqual(all_orders.status_code, 200)
 
-    # def test_empty_order(self):
-    #     """testing for when no order is placed"""
-    #     empty_order = self.app.get('')
-    #     self.assertEqual(empty_order.status_code, 301)
+    def test_empty_order(self):
+        """testing for when no order is placed"""
+        empty_order = self.app.get('')
+        self.assertEqual(empty_order.status_code, 301)
 
-    # def test_get_order(self):
-    #     """fetching only one order test"""
-    #     one_order = self.app.get('api/v1/orders/1')
-    #     self.assertEqual(one_order.status_code, 404)
+    def test_get_order(self):
+        """fetching only one order test"""
+        one_order = self.app.get('api/v1/orders/1')
+        self.assertEqual(one_order.status_code, 200)
 
     def test_adding_order(self):
         """adding an order test"""
